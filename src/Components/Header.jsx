26322ee2fa31bb46396859ef4fa8a31517bef75e
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-07-08 18:19:01 +0530

    Adding The Close Button In Header Responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import "../Styles/Header.css";
 
+
+
 function Header() {
 
   const [openMenu, setOpenMenu] = useState(false);
@@ -14,6 +16,11 @@ function Header() {
         <h3 id="Menu" onClick={ChangeMenu} style={{ display: openMenu ? "none" : "" }}>MENU</h3>
 
         <ul id="links" className={openMenu ? 'show' : 'hide'}>
+          <li className='lists'><p onClick={ChangeMenu}
+          style={{
+            color:"red",
+          }}
+          >X</p></li>
           <li className='lists'><a onClick={ChangeMenu} href="#Main_Home_Div">Home</a></li>
           <li className='lists'><a onClick={ChangeMenu} href="#Main_About_Div">About</a></li>
           <li className='lists'><a onClick={ChangeMenu} href="#Main_Skill_Div">Skills</a></li>
