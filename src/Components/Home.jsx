63e309338c1d0df14f3e9e874b37d879462a12b9
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-07-17 19:12:52 +0530

    17-Jul-2015 Initial Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -49,12 +49,48 @@ function Home() {
         </button> */}
 
         <button id="Resume">
-          <p id='OpenResume'>O</p>
-          <p id='DownloadResume'>D</p>
+          <a href="#" id='OpenResume'>O</a>
+          <a href="#" id='DownloadResume'>D</a>
           Resume
         </button>
 
-      </div>
+
+        <div class="tooltip-container">
+          <span class="text">
+            Resume
+          </span>
+
+          <span class="tooltip2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              class="bi bi-tiktok"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"
+              ></path>
+            </svg>
+          </span>
+          <span class="tooltip3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              class="bi bi-tiktok"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"
+              ></path>
+            </svg>
+          </span>
+
+        </div>
+
+
+      </div >
     </>
   )
 }
