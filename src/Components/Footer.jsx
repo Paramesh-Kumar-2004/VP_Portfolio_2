143b92570e8e95400d4ae757f09131257348ca6b
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-07-21 18:45:20 +0530

    Updating The Portfolio Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <div id='MainFooter'>
             <a href="https://github.com/paramesh-kumar-2004" target='_b'>Github</a>
             <a href="https://www.linkedin.com/in/paramesh-kumar-s-047632249/" target='_b'>Linked In</a>
-            <a href="https://paramesh-kumar-2004.github.io/VP_Portfolio_1/" target='_b'>Portfolio</a>
+            <a href="https://paramesh-kumar-2004.github.io/VP_Portfolio_1/" target='_b'>First Portfolio</a>
         </div>
     )
 }
