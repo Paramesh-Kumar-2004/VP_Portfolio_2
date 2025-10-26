0ffeda06dd864f9776ed82b8d4788e3bfbf0df79
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-26 22:08:13 +0530

    Small Update : Linked In Connections Update

--- src/Components/Social_Media/LinkedIn.jsx
+++ src/Components/Social_Media/LinkedIn.jsx
@@ -15,7 +15,7 @@ function LinkedIn() {
                 <div className="username">Paramesh Kumar S</div>
               </div>
             </div>
-            <div className="about">72 Connections</div>
+            <div className="about">78 Connections</div>
           </div>
         </div>
         <div className="text">
