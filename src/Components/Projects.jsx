769edd48b2f68e993c2a92ef3a2e34f7fdeb44d1
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-12-27 12:54:22 +0530

    Update Project Width

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -22,29 +22,29 @@ function Projects() {
           Project_Link='https://www.google.com'
         />
 
-        {/* <Projects_Component ProjectName='Pyro Patrol' ID_Name='Pyro_Patrol'
-          Description='Designed and developed a fire-detecting autonomous robot using IR sensors and actuators to identify and extinguish fire without human intervention. Programmed using Embedded C/C++ on microcontroller-based hardware, the robot continuously scans its environment, detects heat or flame sources, and activates an extinguishing mechanism, demonstrating intelligent response and automation in fire safety applications.'
-          Project_Domain='IOT'
-          Project_Link='https://www.google.com'
-        />
+          <Projects_Component ProjectName='Pyro Patrol' ID_Name='Pyro_Patrol'
+            Description='Designed and developed a fire-detecting autonomous robot using IR sensors and actuators to identify and extinguish fire without human intervention. Programmed using Embedded C/C++ on microcontroller-based hardware, the robot continuously scans its environment, detects heat or flame sources, and activates an extinguishing mechanism, demonstrating intelligent response and automation in fire safety applications.'
+            Project_Domain='IOT'
+            Project_Link='https://www.google.com'
+          />
 
-        <Projects_Component ProjectName='Brain Friendly' ID_Name='Brain_Friendly'
-          Description='Brain Friendly Is A E-Learning Website Using VR'
-          Project_Domain='Web Interface Development '
-          Project_Link='https://www.google.com'
-        />
+          <Projects_Component ProjectName='Brain Friendly' ID_Name='Brain_Friendly'
+            Description='Brain Friendly Is A E-Learning Website Using VR'
+            Project_Domain='Web Interface Development '
+            Project_Link='https://www.google.com'
+          />
 
-        <Projects_Component ProjectName='Text Based Game' ID_Name='Text_Based_Game'
-          Description="As a Python Developer Intern, developed an interactive text-based adventure game demonstrating core programming concepts such as object-oriented programming, control flow, randomization, and user input handling. Implemented features like role-based character creation, dynamic weapon inventory, and real-time decision trees (e.g., attack, run, treasure) to enhance engagement. Utilized Python's random module for combat logic and custom classes/functions to simulate turn-based gameplay. Included mechanics like life tracking, scoring system, and win/lose conditions, strengthening both logic-building and problem-solving skills."
-          Project_Domain='Game Development (Console Based)'
-          Project_Link='https://github.com/Paramesh-Kumar-2004/Text_Based_Game-Python'
-        />
+          <Projects_Component ProjectName='Text Based Game' ID_Name='Text_Based_Game'
+            Description="As a Python Developer Intern, developed an interactive text-based adventure game demonstrating core programming concepts such as object-oriented programming, control flow, randomization, and user input handling. Implemented features like role-based character creation, dynamic weapon inventory, and real-time decision trees (e.g., attack, run, treasure) to enhance engagement. Utilized Python's random module for combat logic and custom classes/functions to simulate turn-based gameplay. Included mechanics like life tracking, scoring system, and win/lose conditions, strengthening both logic-building and problem-solving skills."
+            Project_Domain='Game Development (Console Based)'
+            Project_Link='https://github.com/Paramesh-Kumar-2004/Text_Based_Game-Python'
+          />
 
-        <Projects_Component ProjectName='Simple AI Voice Assistant' ID_Name='AI_Voice_Assistant'
-          Description='Developed a voice-based personal assistant using Python, integrating pyttsx3, datetime, and pywhatkit libraries to simulate an AI named JARVIS. The assistant features user authentication via password input, time-based greetings, and the ability to search Google or play YouTube videos based on voice-triggered or typed input. Implemented role-based responses for different users and used pyttsx3 for text-to-speech interaction. Demonstrated core Python skills including input handling, conditional logic, function design, and real-time task automation.'
-          Project_Domain='Python Automation'
-          Project_Link='https://github.com/Paramesh-Kumar-2004/Voice_Assistant-Search-Tool'
-        /> */}
+          <Projects_Component ProjectName='Simple AI Voice Assistant' ID_Name='AI_Voice_Assistant'
+            Description='Developed a voice-based personal assistant using Python, integrating pyttsx3, datetime, and pywhatkit libraries to simulate an AI named JARVIS. The assistant features user authentication via password input, time-based greetings, and the ability to search Google or play YouTube videos based on voice-triggered or typed input. Implemented role-based responses for different users and used pyttsx3 for text-to-speech interaction. Demonstrated core Python skills including input handling, conditional logic, function design, and real-time task automation.'
+            Project_Domain='Python Automation'
+            Project_Link='https://github.com/Paramesh-Kumar-2004/Voice_Assistant-Search-Tool'
+          />
 
       </div>
     </div>
