fb3496830e5b419d432fe09f20120e275f55867c
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2026-01-09 20:04:43 +0530

    Project Section Over

--- src/Components/Projects/Projects_Component.jsx
+++ src/Components/Projects/Projects_Component.jsx
@@ -4,7 +4,7 @@ import VP from "../../Images/VP.jpeg";
 
 
 
-function Projects_Component({ ProjectName = "Name None", ID_Name = "None", Description = "Summary None", Project_Domain = "None", Project_Link = "#" }) {
+function Projects_Component({ ProjectName = "Name None", ID_Name = "None", Description = "Summary None", Project_Domain = "None", Source_Code_Frontend = null, Source_Code_Backend = null, Deploy_Link = null, Backend_Name = "Backend" }) {
     return (
         <div className="card">
             <div className="img" id={ID_Name}></div>
@@ -15,9 +15,49 @@ function Projects_Component({ ProjectName = "Name None", ID_Name = "None", Descr
                 &#160;&#160;&#160;{Description}
             </p>
             <p>{Project_Domain}</p>
-            <a href={Project_Link} className="source" target='_b'>
+            {/* <a href={Source_Code_Frontend} className="source" target='_b'>
                 Source Code
-            </a>
+            </a> */}
+
+
+            <div className='All_Source'>
+                {/* Frontend Source Code */}
+                {Source_Code_Frontend && (
+                    <a
+                        href={Source_Code_Frontend}
+                        className="source"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Frontend
+                    </a>
+                )}
+
+                {/* Backend Source Code */}
+                {Source_Code_Backend && (
+                    <a
+                        href={Source_Code_Backend}
+                        className="source"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {Backend_Name}
+                    </a>
+                )}
+
+                {/* Project Deploy Link */}
+                {Deploy_Link && (
+                    <a
+                        href={Deploy_Link}
+                        className="source"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Live Link
+                    </a>
+                )}
+            </div>
+
         </div >
 
     );
