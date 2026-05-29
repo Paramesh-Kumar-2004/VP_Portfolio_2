118e843faa4084b9ee2bfae69a9d126d1c96708f
Paramesh Kumar S <119284841+Paramesh-Kumar-2004@users.noreply.github.com>
2026-05-30 00:24:56 +0530

    Update : App.jsx

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { SnackbarProvider } from 'notistack';
 import { HelmetProvider } from 'react-helmet-async';
 
-
 import App from "./App"
 
 
@@ -20,4 +19,3 @@ root.render(
     </React.StrictMode>
   </SnackbarProvider>
 );
-
